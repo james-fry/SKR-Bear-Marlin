d48aca6a69d8ddd120d5b5399a117e26726cb16e
Chris <codiac2600@gmail.com>
2021-01-10 17:40:06 -0600

    Swapped to LDO 0.9
    LDO 1.68A 0.9 Degree motors

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2264,8 +2264,8 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       600    // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  250    // (mA) RMS current for sensorless homing
+    #define X_CURRENT       950   // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT_HOME  350    // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS      8    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
@@ -2280,8 +2280,8 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       700
-    #define Y_CURRENT_HOME  250
+    #define Y_CURRENT       950
+    #define Y_CURRENT_HOME  350
     #define Y_MICROSTEPS      8
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
@@ -2296,7 +2296,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       450
+    #define Z_CURRENT       400
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2304,7 +2304,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT      450
+    #define Z2_CURRENT      400
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS    16
     #define Z2_RSENSE         0.11
